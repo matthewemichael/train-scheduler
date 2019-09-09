f21d1e2b38886c284921fc6bd3043d9ec51fa811
Matt Michael <matthewemichael@gmail.com>
2019-09-09 12:02:22 -0500

    next train time calculated from current time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
-var config = {
+// Your web app's Firebase configuration
+var firebaseConfig = {
     apiKey: "AIzaSyAZSBPjscSAImrvWt8CWS44RDvJMAh72Z0",
     authDomain: "train-activity-9cfbe.firebaseapp.com",
     databaseURL: "https://train-activity-9cfbe.firebaseio.com",
@@ -7,11 +8,12 @@ var config = {
     messagingSenderId: "412187997189",
     appId: "1:412187997189:web:9b1b63056486c6e17c986c"
 };
-  
-firebase.initializeApp(config);
-  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Create a variable to reference the database
 var database = firebase.database();
-  
+
 // Button for adding trains
 $("#add-train-btn").on("click", function(event) {
     event.preventDefault();
@@ -19,35 +21,16 @@ $("#add-train-btn").on("click", function(event) {
     // Grabs user input
     var trainName = $("#train-name-input").val().trim();
     var trainDestination = $("#destination-input").val().trim();
-    var trainStart = $("#start-input").val().trim();
+    var trainStart = moment($("#start-input").val().trim(), "HH:mm").format("HH:mm");
     var trainFrequency = $("#frequency-input").val().trim();
-
-    // First Train Start, set back 1 year to make sure date comes before current time
-    var trainStartConverted = moment(trainStart, "hh:mm").subtract(1, "years");
-
-    // Difference between times
-    var timeDiff = moment().diff(moment(trainStartConverted), "minutes");
-
-    // Time Apart (Remainder)
-    var timeRemainder = timeDiff % trainFrequency;
-
-    // Minutes Until Train
-    var trainMinAway = trainFrequency - timeRemainder;
-
-    // Next Train
-    var nextTrain = moment().add(trainMinAway, "minutes");
-
-    // Arrival Time
-    var nextArrival = moment(nextTrain).format("hh:mm a");
 
     // Creates local "temporary" object for holding train data
     var newTrain = {
-      name: trainName,
-      destination: trainDestination,
-      start: trainStart,
-      frequency: trainFrequency,
-      minutesAway: trainMinAway,
-      nextArrival: nextArrival,
+      trainName: trainName,
+      trainDestination: trainDestination,
+      trainStart: trainStart,
+      trainFrequency: trainFrequency,
+      date_added: firebase.database.ServerValue.TIMESTAMP
     };
   
     // Uploads train data to the database
@@ -58,7 +41,6 @@ $("#add-train-btn").on("click", function(event) {
     console.log(newTrain.destination);
     console.log(newTrain.start);
     console.log(newTrain.frequency);
-    console.log(newTrain.minutesAway);
     console.log(newTrain.nextArrival);
   
    ////////////////////////////////////
@@ -72,19 +54,40 @@ $("#add-train-btn").on("click", function(event) {
     $("#start-input").val("");
     $("#frequency-input").val("");
 });
-  
+
 // Create Firebase event for adding train to the database and a row in the html when a user adds an entry
 database.ref().on("child_added", function(childSnapshot) {
     console.log(childSnapshot.val());
   
-    // Store everything into a variable.
-    var trainName = childSnapshot.val().name;
-    var trainDestination = childSnapshot.val().destination;
-    var trainStart = childSnapshot.val().start;
-    var trainFrequency = childSnapshot.val().frequency;
-    var nextTrain = childSnapshot.val().nextArrival;
-    var trainMinAway = childSnapshot.val().minutesAway;
-  
+    // Create local variables to store data from firebase
+    var trainName = childSnapshot.val().trainName;
+    var trainDestination = childSnapshot.val().trainDestination;
+    var trainStart = childSnapshot.val().trainStart;
+    var trainFrequency = childSnapshot.val().trainFrequency;
+
+    var trainStartConverted = moment(trainStart, "HH:mm").subtract(1, "years");
+
+    // Difference between times
+    var timeDiff = moment().diff(moment(trainStartConverted), "minutes");
+
+    // Time Apart (Remainder)
+    var timeRemainder = timeDiff % trainFrequency;
+
+    // Minutes Until Train
+    var trainMinAway = trainFrequency - timeRemainder;
+
+    // Next Train
+    var nextTrain = moment().add(trainMinAway, "minutes").format("HH:mm");
+
+    //Conditional to see if train has run yet
+    if (timeRemainder < 0) {
+        nextTrain = trainStart;
+        console.log(nextTrain);
+
+        minToTrain = -timeDiff;
+        console.log(minToTrain);
+    }
+
     // Create the new row
     var newRow = $("<tr>").append(
       $("<td>").text(trainName),
